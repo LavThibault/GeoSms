cc2fc33df8e0337accad1e135e9d2cef78cc1d23
LavThibault <tlavigne@outlook.com>
2016-03-01 16:05:47 +0100

    Starting Project

---
+++ app/src/main/java/iut/projetandroid/elgarrailavigne/geosms/MainActivity.java
@@ -0,0 +1,13 @@
+package iut.projetandroid.elgarrailavigne.geosms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
