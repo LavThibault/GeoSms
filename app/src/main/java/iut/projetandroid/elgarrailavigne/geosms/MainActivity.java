25c187a07f4e56236b7e7227db8a9d81729dc34d
LavThibault <tlavigne@outlook.com>
2016-03-07 20:31:38 +0100

    1

--- app/src/main/java/iut/projetandroid/elgarrailavigne/geosms/MainActivity.java
+++ app/src/main/java/iut/projetandroid/elgarrailavigne/geosms/MainActivity.java
@@ -2,6 +2,9 @@ package iut.projetandroid.elgarrailavigne.geosms;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void affichCalendar(View sender) {
+        ((Button)findViewById(R.id.button)).setVisibility(View.GONE);
+        ((DatePicker)findViewById(R.id.datePicker)).setVisibility(View.VISIBLE);
     }
 }
